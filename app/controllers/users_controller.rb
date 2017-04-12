fae0e3ae12b1dd97472301de7ed390a95dc0bee3
hiro4869 <sakuragima48@gmail.com>
2017-04-12 21:57:59 +0900

    管理ユーザー機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,17 @@
 class UsersController < ApplicationController
   #自分の出品した商品のみ一覧を確認可能
-  # before_action :correct_user, only: [:show]
-  before_action :admin_user, only: [:show]
+  before_action :correct_user, only: [:show]
+  before_action :admin_user, only: [:adminpage]
 
   #ぱんくずリストを表示
   add_breadcrumb "home", :root_path
 
   def show
-    # @user = User.find(params[:id])
-    # @user_products = @user.products
-    @products = Product.all
+    @user = User.find(params[:id])
+    @user_products = @user.products
 
     #パンくずリスト
-    add_breadcrumb "管理者ページ"
+    add_breadcrumb "#{@user.email}さんのマイページ"
   end
 
   def new
@@ -28,14 +27,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def adminpage
+    @products = Product.all
+    #パンくずリスト
+    add_breadcrumb "管理者ページ"
+  end
+
   private
 
-  # def correct_user
-  #   user = User.find(params[:id])
-  #   if current_user.id != user.id
-  #     redirect_to root_path
-  #   end
-  # end
+  def correct_user
+    user = User.find(params[:id])
+    if current_user.id != user.id
+      redirect_to root_path
+    end
+  end
 
   def user_params
     params.require(:user).permit(:email, :password, :password_confirmation)
