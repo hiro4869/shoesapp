171d30b658d44ef7cdfc83e6a69a9e300a0aff58
hiro4869 <sakuragima48@gmail.com>
2017-04-21 02:43:22 +0900

    不正なEメールにバリデーションを実装

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -6,15 +6,17 @@ class Purchase < ApplicationRecord
   # validates :u_name, presence: true
   # validates :u_address, presence: true
   # validates :u_phone_num, presence: true
-  # validates :u_email, presence: true
 
+  #メールに不正な値が入らないようにバリデーション
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :u_email, presence: true, format: { with: VALID_EMAIL_REGEX }
+
+  #購入数のカスタムバリデーション
   validate :purchase_num_is_not_0
 
   #購入数が0ではいけない
   def purchase_num_is_not_0
-    if self.purchase_num == 0
-      errors.add(:purchase_num, '購入数は0ではいけない')
-    end
+      errors.add(:purchase_num, '購入数は0ではいけない') if self.purchase_num == 0
   end
 
 end
