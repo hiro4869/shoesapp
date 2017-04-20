a865d9a60837aea400d01767e311df3631cc3568
hiro4869 <sakuragima48@gmail.com>
2017-04-21 03:27:44 +0900

    不正な電話番号にバリデーションを実装

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -5,14 +5,19 @@ class Purchase < ApplicationRecord
   # validates :purchase_num, presence: true
   # validates :u_name, presence: true
   # validates :u_address, presence: true
-  # validates :u_phone_num, presence: true
+
+  #電話番号に不正な値が入らないようにバリデーション(ハイフン無し半角数字のみ)
+  #ハイフン有りの場合はこちらを使用「/\A\d+-\d+-\d+\z/」 
+  VALID_PHONENUM_REGEX = /\A\d+\z/
+  # validates :u_phone_num, presence: true, format: { with: VALID_PHONENUM_REGEX }
+
 
   #メールに不正な値が入らないようにバリデーション
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  validates :u_email, presence: true, format: { with: VALID_EMAIL_REGEX }
+  # validates :u_email, presence: true, format: { with: VALID_EMAIL_REGEX }
 
   #購入数のカスタムバリデーション
-  validate :purchase_num_is_not_0
+  # validate :purchase_num_is_not_0
 
   #購入数が0ではいけない
   def purchase_num_is_not_0
