053b0cd248604db4d7e2d4baaa13f289e69794e5
hiro4869 <sakuragima48@gmail.com>
2017-04-21 02:06:51 +0900

    購入数が0の場合のvalidation設定

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -2,10 +2,19 @@ class Purchase < ApplicationRecord
   belongs_to :user
   belongs_to :product
 
-  validates :purchase_num, presence: true
-  validates :u_name, presence: true
-  validates :u_address, presence: true
-  validates :u_phone_num, presence: true
-  validates :u_email, presence: true
+  # validates :purchase_num, presence: true
+  # validates :u_name, presence: true
+  # validates :u_address, presence: true
+  # validates :u_phone_num, presence: true
+  # validates :u_email, presence: true
+
+  validate :purchase_num_is_not_0
+
+  #購入数が0ではいけない
+  def purchase_num_is_not_0
+    if self.purchase_num == 0
+      errors.add(:purchase_num, '購入数は0ではいけない')
+    end
+  end
 
 end
