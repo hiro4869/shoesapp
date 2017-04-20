59095dae905c31acc157ebbfbbb551771ff700df
hiro4869 <sakuragima48@gmail.com>
2017-04-21 01:23:48 +0900

    購入明細の文面を作成

--- app/mailers/purchase_mailer.rb
+++ app/mailers/purchase_mailer.rb
@@ -2,7 +2,7 @@ class PurchaseMailer < ApplicationMailer
   default from: "from@example.com"
 
   def purchase_email(email, purchase)
-    @u_name = purchase.u_name
-    mail to: email, subject: "記事を投稿しました"
+    @purchase = purchase
+    mail to: email, subject: "ご購入ありがとうございます"
   end
 end
