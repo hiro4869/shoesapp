a108c98a4c5e134385d200a6b6d1493437f0dfd5
hiro4869 <sakuragima48@gmail.com>
2017-04-21 18:11:58 +0900

    test文原案作成

--- spec/requests/purchases_spec.rb
+++ spec/requests/purchases_spec.rb
@@ -1,21 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Purchase, type: :model do
-  let(:new_purchase) {
-    {
-      u_name: "fukunaga",
-      purchase_num: 5,
-      u_address: "渋谷",
-      u_phone_num: "0907432323",
-      # u_email: "sakurazima48@gmail.com"
-    }
-  }
+  # let(:new_purchase) {
+  #   {
+  #     u_name: "fukunaga",
+  #     purchase_num: 5,
+  #     u_address: "渋谷",
+  #     u_phone_num: "0907432323",
+  #     u_email: "sakurazima48@gmail.com"
+  #   }
+  # }
 
   context "create" do
-    it "購入は通るか" do
-      purchase = Purchase.create! new_purchase
-      expect( 1 + 1 ).to eq 2
-      # expect { Purchase.create! new_purchase }.to change {Purchase.count}.by(1)
+    it "名前と住所が必要" do
+    purchase = Purchase.new(
+      u_name: "fukunaga",
+      # purchase_num: 5,
+      # u_address: "渋谷",
+      # u_phone_num: "0907432323",
+      # u_email: "sakurazima48@gmail.com"
+    )
+    expect(purchase).to be_invalid
     end
   end
 
