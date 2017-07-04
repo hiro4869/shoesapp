17d486bbb8e8998d58f0510b609dfb992961c318
鬼頭杏利 <anri@onigashnrinoMBP.lan>
2017-07-05 00:47:22 +0900

    rootindex,headerを追加

---
+++ public/assets/js/style.js
@@ -0,0 +1,9 @@
+$('body').vegas({
+    slides: [
+        { src: '/assets/images/toppage/footwear-1838767_1920.jpg' },
+        { src: '/assets/images/toppage/StockSnap_HKZLXBRRFP.jpg' },
+        { src: '/assets/images/toppage/leather-926660_1280.jpg' }
+    ],
+    animation: [ 'kenburnsUp', 'kenburnsDown', 'kenburnsLeft', 'kenburnsRight' ]
+
+});
