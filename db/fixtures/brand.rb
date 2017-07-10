b91f7abf31f33ba5bd07cab9eb63dd7d56f81a9c
hiro4869 <sakuragima48@gmail.com>
2017-07-10 14:20:27 +0900

    ブランドモデルとシードデータ追加

---
+++ db/fixtures/brand.rb
@@ -0,0 +1,50 @@
+Brand.seed do |s|
+  s.id = 1
+  s.brand_name = "リーガル"
+end
+
+Brand.seed do |s|
+  s.id = 2
+  s.brand_name = "クロケット＆ジョーンズ"
+end
+
+Brand.seed do |s|
+  s.id = 3
+  s.brand_name = "スコッチグレイン"
+end
+
+Brand.seed do |s|
+  s.id = 4
+  s.brand_name = "ハルタ"
+end
+
+Brand.seed do |s|
+  s.id = 5
+  s.brand_name = "ジャラン スリウァヤ"
+end
+
+Brand.seed do |s|
+  s.id = 6
+  s.brand_name = "コールハーン"
+end
+
+Brand.seed do |s|
+  s.id = 7
+  s.brand_name = "エドワードグリーン"
+end
+
+Brand.seed do |s|
+  s.id = 8
+  s.brand_name = "チャーチ"
+end
+
+Brand.seed do |s|
+  s.id = 9
+  s.brand_name = "ロイドフットウェア"
+end
+
+Brand.seed do |s|
+  s.id = 10
+  s.brand_name = "カルミナ"
+end
+
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170708042644) do
+ActiveRecord::Schema.define(version: 20170710051314) do
+
+  create_table "brands", force: :cascade do |t|
+    t.string   "brand_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "carts", force: :cascade do |t|
     t.integer  "user_id"
