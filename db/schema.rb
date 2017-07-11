00512c65b138a181213f4c04d3d4c814dfd1805f
hiro4869 <sakuragima48@gmail.com>
2017-07-11 18:20:43 +0900

    carts/confirmation.htm.erbのバグを解消

--- db/schema.rb
+++ db/schema.rb
@@ -33,15 +33,6 @@ ActiveRecord::Schema.define(version: 20170711065621) do
     t.datetime "updated_at",    null: false
   end
 
-  create_table "contact_us", force: :cascade do |t|
-    t.string   "title"
-    t.string   "name"
-    t.string   "email"
-    t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "inquiries", force: :cascade do |t|
     t.string   "title"
     t.string   "name"
